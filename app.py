c534b26e7df50f9b81e321d7ed1a2cc433c6c5bf
Santosh148 <skumarsahoo425@gmail.com>
2026-04-09 02:59:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,6 @@ def admin():
     data = cursor.fetchall()
     conn.close()
     return render_template("admin.html", data=data)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
